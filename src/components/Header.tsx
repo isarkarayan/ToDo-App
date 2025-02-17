5af1d91d9fcad48d5bb0ee92450eab05bf9f0d28
isarkarayan <ayansarkarrgj@gmail.com>
2025-02-17 08:52:36 +0530

    Update header logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export function Header() {
     <header className="bg-white shadow">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
         <div className="flex items-center">
-          <CheckSquare className="h-8 w-8 text-blue-500 mr-3" />
+        <a href='https://todo.ayansarkar.co.in/'><CheckSquare className="h-8 w-8 text-blue-500 mr-3" /></a>
           <a href='https://todo.ayansarkar.co.in/'><h1 className="text-2xl font-bold text-gray-900">ToDo Note</h1></a>
         </div>
       </div>
